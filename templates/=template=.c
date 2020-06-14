a8c26930af7748d41203c253c38e8dd88a6e8999
Ruan E. Formigoni <ruanformigoni@gmail.com>
2020-06-14 12:02:36 -0300

    Updated templates formatting

--- templates/=template=.c
+++ templates/=template=.c
@@ -1,11 +1,8 @@
 //
-// %FFILE%
+// File: %FFILE%
 // Copyright (C) %YEAR% %USER% <%MAIL%>
 //
 // Distributed under terms of the %LICENSE% license.
 //
-
-#include "%FILE%.h"
-
 
 %HERE%
--- templates/=template=.h
+++ templates/=template=.h
@@ -1,5 +1,5 @@
 //
-// %FFILE%
+// File: %FFILE%
 // Copyright (C) %YEAR% %USER% <%MAIL%>
 //
 // Distributed under terms of the %LICENSE% license.
